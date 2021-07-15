2a0ca9ef597f601a882c8b9b69a6e95e493e613b
sorryanot <eluk1991@gmail.com>
2021-07-15 17:49:20 +0300

    added loading data from file "json"

--- addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -14,24 +18,30 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private String browser;
+    private final Properties properties;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+
+    public void init() throws IOException {
+        String target=System.getProperty("target","local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
         }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         contactHelper = new ContactHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
--- addressbook-web-tests/src/test/java/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/TestBase.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.IOException;
+
 public class TestBase {
 
-    public static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    public static final ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
-    public void setUp() {
+    public void setUp() throws IOException {
         app.init();
     }
 
