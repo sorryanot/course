3649a720c46b90ae0ed269444b7ec93e1109a71a
sorryanot <eluk1991@gmail.com>
2021-07-16 14:17:51 +0300

    connect bd

--- addressbook-web-tests/src/test/java/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/addressbook/model/GroupData.java
@@ -1,17 +1,30 @@
 package addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
+@Entity
+@Table(name = "group_list")
 public class GroupData {
-
+    @Id
+    @Column(name = "group_id")
     private int id = Integer.MAX_VALUE;
     @Expose
+    @Column(name = "group_name")
     private String name;
     @Expose
+    @Column(name = "group_header")
+    @Type(type ="text")
     private String header;
     @Expose
+    @Column(name = "group_footer")
+    @Type(type ="text")
     private String footer;
 
     public GroupData withId(int id) {
